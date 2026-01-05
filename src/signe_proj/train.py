f597d7afe39e13a68afcddb14900fd1eca3b82d9
Signe Djernis <signedjernis@gmail.com>
2026-01-05 16:23:11 +0100

    new env and cookiecutter

---
+++ src/signe_proj/train.py
@@ -0,0 +1,10 @@
+from signe_proj.model import Model
+from signe_proj.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
