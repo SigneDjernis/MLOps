418c39aa07893ec3e27b6221b13c064e81660bb3
Signe Djernis <signedjernis@gmail.com>
2026-01-05 15:53:26 +0100

    first model setup

---
+++ model.py
@@ -0,0 +1,24 @@
+from torch import nn
+
+class MyAwesomeModel(nn.Module):
+    """Basic Neural Network with 1 hidden layer."""
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Inputs to hidden layer linear transformation
+        self.hidden = nn.Linear(784, 256)
+        # Output layer, 10 units - one for each class
+        self.output = nn.Linear(256, 10)
+
+        # Define sigmoid activation and softmax output
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        """Forward pass through the network, returns the output logits."""
+        # Pass the input tensor through each of our operations
+        x = self.hidden(x)
+        x = self.sigmoid(x)
+        x = self.output(x)
+        return self.softmax(x)
