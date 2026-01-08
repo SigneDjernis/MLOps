709588879bff5bf4d2da71dd23e517c10cfba697
Signe Djernis <signedjernis@gmail.com>
2026-01-08 15:45:11 +0100

    feat wandb

--- src/signe_proj/train.py
+++ src/signe_proj/train.py
@@ -1,21 +1,22 @@
 import matplotlib.pyplot as plt
-from signe_proj.model import MyAwesomeModel
-from signe_proj.data import corrupt_mnist
-import typer
 import torch
+import typer
+import wandb
+from signe_proj.data import corrupt_mnist
+from signe_proj.model import MyAwesomeModel
+from sklearn.metrics import RocCurveDisplay, accuracy_score, f1_score, precision_score, recall_score
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
-    """Train a model on MNIST.
-    Inputs: lr: learning rate
-    batch_size: batch size
-    epochs: number of epochs
-    outputs: None
-    """
+def train(lr: float = 0.001, batch_size: int = 32, epochs: int = 5) -> None:
+    """Train a model on MNIST."""
     print("Training day and night")
     print(f"{lr=}, {batch_size=}, {epochs=}")
+    run = wandb.init(
+        project="corrupt_mnist",
+        config={"lr": lr, "batch_size": batch_size, "epochs": epochs},
+    )
 
     model = MyAwesomeModel().to(DEVICE)
     train_set, _ = corrupt_mnist()
@@ -25,35 +26,68 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
-    statistics = {"train_loss": [], "train_accuracy": []}
     for epoch in range(epochs):
         model.train()
+
+        preds, targets = [], []
         for i, (img, target) in enumerate(train_dataloader):
             img, target = img.to(DEVICE), target.to(DEVICE)
             optimizer.zero_grad()
-            img_flat = img.view(img.size(0), -1)
+            img_flat = img.view(img.size(0), -1)  # flatten the images
             y_pred = model(img_flat)
             loss = loss_fn(y_pred, target)
             loss.backward()
             optimizer.step()
-            statistics["train_loss"].append(loss.item())
-
             accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
-            statistics["train_accuracy"].append(accuracy)
+            wandb.log({"train_loss": loss.item(), "train_accuracy": accuracy})
+
+            preds.append(y_pred.detach().cpu())
+            targets.append(target.detach().cpu())
 
             if i % 100 == 0:
                 print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
 
-    print("Training complete")
-    # save in folder models/
-    torch.save(model.state_dict(), "models/model.pth")
-    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
-    axs[0].plot(statistics["train_loss"])
-    axs[0].set_title("Train loss")
-    axs[1].plot(statistics["train_accuracy"])
-    axs[1].set_title("Train accuracy")
-    # save in folder reports/figures/
-    fig.savefig("reports/figures/training_statistics.png")
+                # add a plot of the input images
+                images = [wandb.Image(img[j].detach().cpu(), caption=f"Image {j}") for j in range(min(5, img.size(0)))]
+                wandb.log({"images": images})
+
+                # add a plot of histogram of the gradients
+                grads = torch.cat([p.grad.flatten() for p in model.parameters() if p.grad is not None], 0)
+                wandb.log({"gradients": wandb.Histogram(grads)})
+
+        # add a custom matplotlib plot of the ROC curves
+        preds = torch.cat(preds, 0)
+        targets = torch.cat(targets, 0)
+
+        for class_id in range(10):
+            one_hot = torch.zeros_like(targets)
+            one_hot[targets == class_id] = 1
+            _ = RocCurveDisplay.from_predictions(
+                one_hot,
+                preds[:, class_id],
+                name=f"ROC curve for {class_id}",
+                plot_chance_level=(class_id == 2),
+            )
+
+        # alternatively use wandb.log({"roc": wandb.Image(plt)}
+        wandb.log({"roc": wandb.Image(plt)})
+        plt.close()  # close the plot to avoid memory leaks and overlapping figures
+
+    final_accuracy = accuracy_score(targets, preds.argmax(dim=1))
+    final_precision = precision_score(targets, preds.argmax(dim=1), average="weighted")
+    final_recall = recall_score(targets, preds.argmax(dim=1), average="weighted")
+    final_f1 = f1_score(targets, preds.argmax(dim=1), average="weighted")
+
+    # first we save the model to a file then log it as an artifact
+    torch.save(model.state_dict(), "model.pth")
+    artifact = wandb.Artifact(
+        name="corrupt_mnist_model",
+        type="model",
+        description="A model trained to classify corrupt MNIST images",
+        metadata={"accuracy": final_accuracy, "precision": final_precision, "recall": final_recall, "f1": final_f1},
+    )
+    artifact.add_file("model.pth")
+    run.log_artifact(artifact)
 
 
 if __name__ == "__main__":
